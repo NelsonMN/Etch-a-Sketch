5fb7cab5291650e1df5ec74c0d7537e6454ebf89
Nelson <nelson.mieszkalski@gmail.com>
2022-01-10 18:14:32 -0500

    Added a UI with buttons to change colours

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// adding 16x16 grid of square divs
-
+// size X size grid of square divs
 function createRows(size) {
     for (let i = 0; i < size; i++) {
         const container = document.querySelector('.container');
@@ -16,10 +15,97 @@ function createColumns(size) {
         for (let i = 0; i < size; i++) {
             const div = document.createElement('div');
             div.classList.add('column-square');
-            row.append(div)
+            row.append(div);
     }
 }}
 
 
-createRows(4)
-createColumns(4)
+// Grid Deletion
+function deleteGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+
+// Creating a new grid after size input
+function createGrid(size) {
+    createRows(size);
+    createColumns(size);
+}
+
+function getGridSize() {
+    size = prompt("What size grid would you like to use?");
+    while (isNaN(size) || size < 2 || size > 50) {
+        size = prompt("What size grid would you like to use?");
+    }
+    createGrid(size);
+};
+
+
+// Reset button
+function reset(element) {
+    const container = document.querySelector('.container');
+    const squares = Array.from(document.querySelectorAll('.column-square'))
+    squares.forEach(function(element) {
+        element.style.backgroundColor = 'white';
+    })
+    deleteGrid(container);
+    getGridSize();
+}
+
+// Erase Button
+function erase() {
+    const squares = Array.from(document.querySelectorAll('.column-square'))
+    squares.forEach(function(element) {
+        element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'white';}
+            )})
+}
+
+
+// MouseOut Event Listener - Black
+function drawBlack() {
+    const squares = Array.from(document.querySelectorAll('.column-square'))
+    squares.forEach(function(element) {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = 'black';}
+        )})
+}
+
+
+// Grabbing Random Colours
+function randomColour() {
+    const x = Math.round 
+    const y = Math.random
+    const z = 255;
+    return 'rgba(' + x(y()*z) + ',' + x(y()*z) + ',' + x(y()*z) + ',' + y().toFixed(1) + ')';
+}
+
+
+// MouseOut Event Listener - Rainbow
+function drawRainbow() {
+    const squares = Array.from(document.querySelectorAll('.column-square'))
+    squares.forEach(function(element) {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = randomColour();}
+        )})
+}
+
+
+// MouseOut Event Listener - Greyscale
+function greyScale() {
+    const squares = Array.from(document.querySelectorAll('.column-square'))
+    squares.forEach(function(element) {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = 'black';
+            let opacity = Number(element.style.opacity);
+            opacity += 0.1;
+            element.style.opacity = opacity;
+        })})
+    }
+
+
+// Default Starting Grid
+createRows(10);
+createColumns(10);
